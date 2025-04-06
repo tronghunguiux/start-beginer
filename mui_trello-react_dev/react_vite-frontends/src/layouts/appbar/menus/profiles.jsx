c94eb40c58ceb90f9594c4bd64b00fcf5937e186
tronghunguiux <tronghung.uiux@gmail.com>
2025-04-06 15:00:19 +0700

    update board bar navigation

--- mui_trello-react_dev/react_vite-frontends/src/layouts/appbar/menus/profiles.jsx
+++ mui_trello-react_dev/react_vite-frontends/src/layouts/appbar/menus/profiles.jsx
@@ -2,12 +2,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import React from 'react'
-import { Avatar, Box, Button, Divider, IconButton, Menu, MenuItem, Tooltip } from '@mui/material'
-import Fade from '@mui/material/Fade'
-import { Check, Logout, PersonAdd, Settings } from '@mui/icons-material'
-import ListItemText from '@mui/material/ListItemText'
+import { Avatar, Box, Divider, IconButton, Menu, MenuItem, Tooltip } from '@mui/material'
+import { Logout, PersonAdd, Settings } from '@mui/icons-material'
 import ListItemIcon from '@mui/material/ListItemIcon'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 export default function ProfileBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -22,79 +19,78 @@ export default function ProfileBar() {
     <>
       <Box>
         <div>
-            
-            <Tooltip title="Account settings">
-                <IconButton
-                    onClick={handleClick}
-                    size="small"
-                    sx={{  }}
-                    aria-controls={open ? 'fade-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}>
-                    <Avatar sx={{ width: 32, height: 32 }}>H</Avatar>
-                </IconButton>
-            </Tooltip>
+          <Tooltip title="Account settings">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{}}
+              aria-controls={open ? 'fade-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}>
+              <Avatar sx={{ width: 32, height: 32 }}>H</Avatar>
+            </IconButton>
+          </Tooltip>
 
-            <Menu
-        anchorEl={anchorEl}
-        id="fade-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&::before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem onClick={handleClose}>
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
+          <Menu
+            anchorEl={anchorEl}
+            id="fade-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                mt: 1.5,
+                '& .MuiAvatar-root': {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                '&::before': {
+                  content: '""',
+                  display: 'block',
+                  position: 'absolute',
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: 'background.paper',
+                  transform: 'translateY(-50%) rotate(45deg)',
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Avatar sx={{ width: '34px', height: '34px', fontSize: '1rem' }} /> Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Avatar sx={{ width: '34px', height: '34px', fontSize: '1rem' }} /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <PersonAdd fontSize="small" />
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
           </Menu>
         </div>
       </Box>
