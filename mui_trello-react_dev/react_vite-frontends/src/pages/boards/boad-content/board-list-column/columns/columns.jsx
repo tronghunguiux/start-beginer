d43d9a24d9bb4cbbde4c234ba0f87c853a487237
tronghunguiux <tronghung.uiux@gmail.com>
2025-04-09 23:35:51 +0700

    update draggable

--- mui_trello-react_dev/react_vite-frontends/src/pages/boards/boad-content/board-list-column/columns/columns.jsx
+++ mui_trello-react_dev/react_vite-frontends/src/pages/boards/boad-content/board-list-column/columns/columns.jsx
@@ -1,40 +1,51 @@
 import React from 'react'
 import { Box, Divider, Menu, MenuItem, Typography, Fade, ListItemText, ListItemIcon, Tooltip, Button } from '@mui/material'
-import { Card, CardActions, CardContent, CardMedia } from '@mui/material';
 
 import { ContentCut, ContentCopy, ContentPaste, Cloud } from '@mui/icons-material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddCardIcon from '@mui/icons-material/AddCard';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
-import GroupIcon from '@mui/icons-material/Group';
-import ModeCommentIcon from '@mui/icons-material/ModeComment';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
 import ListCard from './list-card/list-card';
 import { mapOrder } from '~/utils/sorts'
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 
 const COLUMN_H_HEADER = (theme) => (theme.trello.columnHeaderHeight);
 const COLUMN_H_FOOTER = (theme) => (theme.trello.columnFooterHeight);
 
 export default function BoardColumn({column}) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: column._id,
+    data: { ...column },
+  })
+    
+  const DndKitColumnStyles = {
+    touchAction: 'none',
+    transform: CSS.Translate.toString(transform),
+    transition,
+  }
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => { setAnchorEl(event.currentTarget) };
   const handleClose = () => { setAnchorEl(null) };
 
-    const orderCard = mapOrder(column?.cards, column?.cardOrderIds, '_id')
+  const orderCard = mapOrder(column?.cards, column?.cardOrderIds, '_id')
   
   return (
     <>
-      <Box sx={{
-        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
-        minWidth: '300px',
-        maxWidth: '300px',
-        ml: 2,
-        height: 'fit-content',
-        borderRadius: '8px',
-        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
-      }}>
+      <Box ref={setNodeRef} style={DndKitColumnStyles} {...attributes} {...listeners} 
+        sx={{
+          backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+          minWidth: '300px',
+          maxWidth: '300px',
+          ml: 2,
+          height: 'fit-content',
+          borderRadius: '8px',
+          maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+        }}
+      >
         {/* Header */}
         <Box sx={{
           width: '100%',
